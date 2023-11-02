37679d27d35fbcf114e1fa7b9c1cbc1d8485257a
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-02 10:30:18 +0200

    create project

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CopyRight from "./components/CopyRight";
+import Bouquets from "./components/Bouquets";
+import Decoration from "./components/Decoration";
+import GiftBoxes from "./components/GiftBoxes";
+
+function App() {
+  
+  return (
+    <div>
+      <Header />         
+      <Bouquets />      
+      <Decoration />      
+      <GiftBoxes />      
+      <Footer />          
+      <CopyRight />      
+    </div>    
+  );
+}
+
+export default App;
