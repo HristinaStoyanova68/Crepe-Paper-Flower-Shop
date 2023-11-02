5e574a579e2c84e59af46db6e4bc1395c28f6482
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-02 10:31:45 +0200

    add Bouquets component

---
+++ client/src/components/Bouquets.jsx
@@ -0,0 +1,142 @@
+export default function Bouquets () {
+    return (
+        <div className="fashion_section">
+         <div id="main_slider" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner">
+               <div className="carousel-item active">
+                  <div className="container">
+                     <h1 className="fashion_taital">BOUQUETS</h1>
+                     <div className="fashion_section_2">
+                        <div className="row">
+                           <div className="col-lg-4 col-sm-4">
+                              <div className="box_main">
+                                 <h4 className="shirt_text">Man T -shirt</h4>
+                                 <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
+                                 <div className="tshirt_img"><img src="images/tshirt-img.png"/></div>
+                                 <div className="btn_main">
+                                    <div className="buy_bt"><a href="#">Buy Now</a></div>
+                                    <div className="seemore_bt"><a href="#">See More</a></div>
+                                 </div>
+                              </div>
+                           </div>
+                           <div className="col-lg-4 col-sm-4">
+                              <div className="box_main">
+                                 <h4 className="shirt_text">Man -shirt</h4>
+                                 <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
+                                 <div className="tshirt_img"><img src="images/dress-shirt-img.png"/></div>
+                                 <div className="btn_main">
+                                    <div className="buy_bt"><a href="#">Buy Now</a></div>
+                                    <div className="seemore_bt"><a href="#">See More</a></div>
+                                 </div>
+                              </div>
+                           </div>
+                           <div className="col-lg-4 col-sm-4">
+                              <div className="box_main">
+                                 <h4 className="shirt_text">Woman Scart</h4>
+                                 <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
+                                 <div className="tshirt_img"><img src="images/women-clothes-img.png"/></div>
+                                 <div className="btn_main">
+                                    <div className="buy_bt"><a href="#">Buy Now</a></div>
+                                    <div className="seemore_bt"><a href="#">See More</a></div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div className="carousel-item">
+                  <div className="container">
+                     <h1 className="fashion_taital">Something Else</h1>
+                     <div className="fashion_section_2">
+                        <div className="row">
+                           <div className="col-lg-4 col-sm-4">
+                              <div className="box_main">
+                                 <h4 className="shirt_text">Man T -shirt</h4>
+                                 <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
+                                 <div className="tshirt_img"><img src="images/tshirt-img.png"/></div>
+                                 <div className="btn_main">
+                                    <div className="buy_bt"><a href="#">Buy Now</a></div>
+                                    <div className="seemore_bt"><a href="#">See More</a></div>
+                                 </div>
+                              </div>
+                           </div>
+                           <div className="col-lg-4 col-sm-4">
+                              <div className="box_main">
+                                 <h4 className="shirt_text">Man -shirt</h4>
+                                 <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
+                                 <div className="tshirt_img"><img src="images/dress-shirt-img.png"/></div>
+                                 <div className="btn_main">
+                                    <div className="buy_bt"><a href="#">Buy Now</a></div>
+                                    <div className="seemore_bt"><a href="#">See More</a></div>
+                                 </div>
+                              </div>
+                           </div>
+                           <div className="col-lg-4 col-sm-4">
+                              <div className="box_main">
+                                 <h4 className="shirt_text">Woman Scart</h4>
+                                 <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
+                                 <div className="tshirt_img"><img src="images/women-clothes-img.png" /></div>
+                                 <div className="btn_main">
+                                    <div className="buy_bt"><a href="#">Buy Now</a></div>
+                                    <div className="seemore_bt"><a href="#">See More</a></div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div className="carousel-item">
+                  <div className="container">
+                     <h1 className="fashion_taital">Something Else</h1>
+                     <div className="fashion_section_2">
+                        <div className="row">
+                           <div className="col-lg-4 col-sm-4">
+                              <div className="box_main">
+                                 <h4 className="shirt_text">Man T -shirt</h4>
+                                 <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
+                                 <div className="tshirt_img"><img src="images/tshirt-img.png" /></div>
+                                 <div className="btn_main">
+                                    <div className="buy_bt"><a href="#">Buy Now</a></div>
+                                    <div className="seemore_bt"><a href="#">See More</a></div>
+                                 </div>
+                              </div>
+                           </div>
+                           <div className="col-lg-4 col-sm-4">
+                              <div className="box_main">
+                                 <h4 className="shirt_text">Man -shirt</h4>
+                                 <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
+                                 <div className="tshirt_img"><img src="images/dress-shirt-img.png" /></div>
+                                 <div className="btn_main">
+                                    <div className="buy_bt"><a href="#">Buy Now</a></div>
+                                    <div className="seemore_bt"><a href="#">See More</a></div>
+                                 </div>
+                              </div>
+                           </div>
+                           <div className="col-lg-4 col-sm-4">
+                              <div className="box_main">
+                                 <h4 className="shirt_text">Woman Scart</h4>
+                                 <p className="price_text">Price  <span style={{color: '#262626'}}>$ 30</span></p>
+                                 <div className="tshirt_img"><img src="images/women-clothes-img.png" /></div>
+                                 <div className="btn_main">
+                                    <div className="buy_bt"><a href="#">Buy Now</a></div>
+                                    <div className="seemore_bt"><a href="#">See More</a></div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <a className="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
+            <i className="fa fa-angle-left"></i>
+            </a>
+            <a className="carousel-control-next" href="#main_slider" role="button" data-slide="next">
+            <i className="fa fa-angle-right"></i>
+            </a>
+         </div>
+      </div>
+    );
+}
