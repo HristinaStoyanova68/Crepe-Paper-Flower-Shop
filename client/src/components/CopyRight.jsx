4db537a64201101131ea1d8b607b0a3c6ede40d1
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-02 10:41:10 +0200

    add CopyRight component

---
+++ client/src/components/CopyRight.jsx
@@ -0,0 +1,9 @@
+export default function CopyRight () {
+    return (
+        <div className="copyright_section">
+         <div className="container">
+            <p className="copyright_text">© 2020 All Rights Reserved. Design by <a href="https://html.design">Free html  Templates</a></p>
+         </div>
+      </div>
+    );
+}
