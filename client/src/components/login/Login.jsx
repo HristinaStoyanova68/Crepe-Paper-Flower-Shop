73a0d1e4b7725aaa53ba65742d2e69705ff1a6a0
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-22 14:04:11 +0200

    change login to controlled form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,25 +1,38 @@
 import { Link } from 'react-router-dom';
 import styles from './Login.module.css';
 
+import useForm from '../../hooks/useForm';
+
 export default function Login() {
-    return (
-        <section className="login">
-    <div className={styles.form}>
-      <h2>Login</h2>
-      <form className={styles["login-form"]}>
-        <input type="text" name="email" id="email" placeholder="email" />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="password"
-        />
-        <button type="submit">login</button>
-        <p className={styles.message}>
-          Not registered? <Link to="/register">Create an account</Link>
-        </p>
-      </form>
-    </div>
-  </section>
-    );
+  const { values, onChange, onSubmit } = useForm();
+
+  return (
+    <section className="login">
+      <div className={styles.form}>
+        <h2>Login</h2>
+        <form className={styles["login-form"]} onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="email"
+            onChange={onChange}
+            value={values["email"]}
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+            onChange={onChange}
+            value={values["password"]}
+          />
+          <button type="submit">login</button>
+          <p className={styles.message}>
+            Not registered? <Link to="/register">Create an account</Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 }
