78bb1c13cecd8eba0d21089c028f8f908edb6b3d
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-22 19:44:38 +0200

    add authService

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+
+import AuthContext from './contexts/authContext';
+import * as authService from './services/authService';
 
 
 import Navbar from './components/Navbar';
@@ -13,29 +16,44 @@ import ItemDetails from './components/item-details/ItemDetails';
 import Collections from './components/collections/Collections';
 import ItemCreate from './components/item-create/ItemCreate';
 import ItemEdit from './components/item-edit/ItemEdit';
+import Path from './paths';
 
 function App() {
+  const navigate = useNavigate();
   const [auth, setAuth] = useState({});
 
-  const loginSubmitHandler = (values) => {
+  const loginSubmitHandler = async (values) => {
     console.log(values);
+    const {
+      accessToken,
+      email,
+      username,
+      _id,
+    } = await authService.login(values.email, values.password);
+
+    setAuth({
+      accessToken,
+      email,
+      username,
+      _id,
+    });
+
+    navigate(Path.Home);
   }
 
   return (
-
-    <>
+    <AuthContext.Provider value={{ loginSubmitHandler }}>
       <div className="banner_bg_main">
         <Navbar />
 
         <HeaderSection />
-
 
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/bouquets' element={<Collections />} />
           <Route path='/decorations' element={<Collections />} />
           <Route path='/gift-boxes' element={<Collections />} />
-          <Route path='/login' element={<Login loginSubmitHandler={loginSubmitHandler}/>} />
+          <Route path='/login' element={<Login loginSubmitHandler={loginSubmitHandler} />} />
           <Route path='/register' element={<Register />} />
           <Route path='/:collectionName/:itemId/details' element={<ItemDetails />} />
           <Route path='/create' element={<ItemCreate />} />
@@ -43,12 +61,10 @@ function App() {
         </Routes>
       </div>
 
-      
-
       <Footer />
 
       <CopyRight />
-    </>
+    </AuthContext.Provider>
   );
 }
 
