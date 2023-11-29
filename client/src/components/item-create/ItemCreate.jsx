9b074f6324fb133be16e745ed5e3428479f534e6
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-11-29 17:22:59 +0200

    set create form to  controlled form

--- client/src/components/item-create/ItemCreate.jsx
+++ client/src/components/item-create/ItemCreate.jsx
@@ -3,56 +3,86 @@ import { useNavigate } from 'react-router-dom';
 import styles from './ItemCreate.module.css';
 
 import * as itemService from '../../services/itemsService';
+import useForm from '../../hooks/useForm';
+import Path from '../../paths';
+
+const createFormKeys = {
+    collectionName: 'collectionName',
+    name: 'name',
+    price: 'price',
+    imageUrl: 'imageUrl',
+    description: 'description',
+}
 
 export default function ItemCreate() {
     const navigate = useNavigate();
 
-    const createItemSubmitHandler = async (e) => {
-        e.preventDefault();
+    const createItemSubmitHandler = async (values) => {
 
-        const itemData = Object.fromEntries(new FormData(e.currentTarget));
 
-        const collectionName = itemData.collectionName;
+        const collectionName = values.collectionName;
+        // console.log(itemData);
+        console.log(collectionName);
 
         try {
-            await itemService.create(collectionName, itemData);
-            navigate('/');
+            await itemService.create(collectionName, values);
+            navigate(Path.Home);
         } catch (err) {
             console.log(err);
         }
     }
+
+    const {values, onChange, onSubmit} = useForm(createItemSubmitHandler, {
+        [createFormKeys.collectionName]: '',
+        [createFormKeys.name]: '',
+        [createFormKeys.price]: '',
+        [createFormKeys.imageUrl]: '',
+        [createFormKeys.description]: '',
+
+    });
+
     return (
 
         // <!-- Create Page (Only for logged-in users) -->
         <section className="create">
             <div className={styles.form}>
                 <h2>Add Item</h2>
-                <form className={styles["create-form"]} onSubmit={createItemSubmitHandler}>
+                <form className={styles["create-form"]} onSubmit={onSubmit}>
                     <input
                         type="text"
                         name="collectionName"
                         placeholder="collectionName"
+                        value={values[createFormKeys.collectionName]}
+                        onChange={onChange}
                     />
                     <input
                         type="text"
                         name="name"
                         placeholder="name"
+                        value={values[createFormKeys.name]}
+                        onChange={onChange}
                     />
                     <input
                         type="text"
                         name="price"
                         placeholder="price"
+                        value={values[createFormKeys.price]}
+                        onChange={onChange}
                     />
                     <input
                         type="text"
                         name="imageUrl"
                         placeholder="Image URL"
+                        value={values[createFormKeys.imageUrl]}
+                        onChange={onChange}
                     />
                     <textarea
                         name="description"
                         placeholder="description"
                         rows="10"
                         cols="50"
+                        value={values[createFormKeys.description]}
+                        onChange={onChange}
                     ></textarea>
                     <button type="submit">Add Item</button>
                 </form>
