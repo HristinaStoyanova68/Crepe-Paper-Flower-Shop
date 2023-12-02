0fb975f6a7142b7a5dc7139f94c5d41f23a9bc71
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-12-02 15:44:56 +0200

    update footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,19 +1,19 @@
+import { Link } from "react-router-dom";
+import Path from "../../paths";
+
 export default function Footer () {
     return (
         <div className="footer_section layout_padding">
-         <div className="container">
-            <div className="footer_logo"><a href="index.html">hristina&apos;s craft world</a></div>
+         <div className="container" style={{display: "flex", justifyContent: "space-between"}}>
+            <div className="footer_logo"><Link to={Path.Home}>hristina&apos;s craft world</Link></div>
             
             <div className="footer_menu">
                <ul>
-                  <li><a href="#">Best Sellers</a></li>
-                  <li><a href="#">Gift Ideas</a></li>
-                  <li><a href="#">New Releases</a></li>
-                  <li><a href="#">Deals for Today</a></li>
-                  <li><a href="#">Contact Us</a></li>
+                  <li><Link to={Path.NewReleases}>New Releases</Link></li>
+                  <li><Link to={Path.AboutUs}>About Us</Link></li>
+                  <li><Link to={Path.ContactUs}>Contact Us</Link></li>
                </ul>
             </div>
-            {/* <div className="location_main">Help Line  Number : <a href="#">+1 1800 1200 1200</a></div> */}
          </div>
       </div>
     );
--- client/src/paths.js
+++ client/src/paths.js
@@ -1,5 +1,7 @@
 const Path = {
     Home: '/',
+    AboutUs: '/about-us',
+    ContactUs: '/contact-us',
     Register: '/register',
     Login: '/login',
     Logout: '/logout',
@@ -8,8 +10,9 @@ const Path = {
     Collection_Gift_Boxes: '/gift_boxes',
     ItemCreate: '/create',
     ItemDetails: '/:collectionName/:itemId/details',
-    ItemEdit: "/:collectionName/:itemId/edit",
-    ItemRemove: "/:collectionName/:itemId/remove",
+    ItemEdit: '/:collectionName/:itemId/edit',
+    ItemRemove: '/:collectionName/:itemId/remove',
+    NewReleases: '/new-releases',
 };
 
 export default Path;
