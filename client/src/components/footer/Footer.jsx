2172444c6d6ccbe97d9300cb6eecd0ca84c93dc9
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-12-02 19:46:02 +0200

    extract Footer.module.css

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import Path from "../../paths";
 
-export default function Footer () {
+import styles from './Footer.module.css';
+
+export default function Footer() {
     return (
-        <div className="footer_section layout_padding">
-         <div className="container" style={{display: "flex", justifyContent: "space-between"}}>
-            <div className="footer_logo"><Link to={Path.Home}>hristina&apos;s craft world</Link></div>
+        <div className={`${styles["footer_section"]} ${styles["layout_padding"]}`}>
+         <div className="container" style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+            <div className={styles["footer_logo"]}><Link to={Path.Home}>hristina&apos;s craft world</Link></div>
             
-            <div className="footer_menu">
+            <div className={styles["footer_menu"]}>
                <ul>
                   <li><Link to={Path.NewReleases}>New Releases</Link></li>
                   <li><Link to={Path.AboutUs}>About Us</Link></li>
