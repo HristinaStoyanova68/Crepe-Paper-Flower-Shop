9f73a8172b6394698a36337bbd707bdd791856d5
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-12-03 00:19:05 +0200

    extract UserMenu.module.css

--- client/src/components/header-section/UserMenu.jsx
+++ client/src/components/header-section/UserMenu.jsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
 import Path from '../../paths';
 
+import styles from './UserMenu.module.css';
+
 export default function UserMenu() {
     return (
-        <div className="header_box">
+        <div className={styles["header_box"]}>
 
-            <div className="login_menu">
+            <div className={styles["login_menu"]}>
                 <Link to={Path.MyProfile}>
                     <i className="fa fa-user" aria-hidden="true"></i>
-                    <span className="padding_10">Profile</span>
+                    <span className={styles["padding_10"]}>Profile</span>
                 </Link>
             </div>
         </div>
