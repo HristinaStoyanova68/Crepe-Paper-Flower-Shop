0ff7e276af9d27487047c39add63051c048cfed4
HristinaStoyanova68 <117320666+HristinaStoyanova68@users.noreply.github.com>
2023-12-03 16:07:45 +0200

    create contact us page

---
+++ client/src/components/footer/contact-us/ContactUs.jsx
@@ -0,0 +1,23 @@
+import styles from './ContactUs.module.css';
+
+export default function ContactUs() {
+    return (
+        <>
+            <div className={styles.post}>
+                <p>
+                Dvanadeseta Street, Tsarevets
+                <br />
+                Bulgaria
+                </p>
+
+                <p>
+                +359 899 008680
+                </p>
+
+                <p>
+                viara68@abv.bg
+                </p>
+            </div>
+        </>
+    );
+}
